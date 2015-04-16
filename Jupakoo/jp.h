e67dbc50516b8f1b06c60365715717cc97935414
MinsEcho <Ian.burns451@gmail.com>
2015-04-16 19:21:16 -0400

    Clean up header files

--- Jupakoo/jp.h
+++ Jupakoo/jp.h
@@ -2,27 +2,8 @@
 #ifndef _JP_H_
 #define _JP_H_
 
-#define leftWheel 3   	// motor
-#define rightWheel 2  	// motor
-#define clawPort 1 	 	// servo
-#define armPort 2  	  	// servo
-#define dumpPort 3  	// servo
-
-#define lightPort 0   	// infrared analog
-#define topHatPort 2  	// analog
-#define touchPort 15  	// analog
-
-// Directions
-#define L 0
-#define R 1
-#define F 2
-#define B 3
-
 #define openClaw 600
 #define closeClaw 1400
-
-//set_servo_position()
-#define ssp(port,position) set_servo_position(port,position) 
 
 void setup();
 
