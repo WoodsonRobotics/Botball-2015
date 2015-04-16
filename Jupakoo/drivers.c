15b2b6bfa23028dc79d9d151a069de096e61ace0
MinsEcho <Ian.burns451@gmail.com>
2015-04-16 19:35:10 -0400

    kctyk'.

--- Jupakoo/drivers.c
+++ Jupakoo/drivers.c
@@ -20,13 +20,17 @@ void turn(int speed, int angle)
 	set_create_total_angle(0);
 	if(angle<0){
 		create_spin_CW(speed);
+		
 		while(get_create_total_angle() > angle)
 			printf("%d degrees turned\n", get_create_total_angle());
+		
 		create_stop();
 	}else{
 		create_spin_CCW(speed);
+		
 		while(get_create_total_angle() < angle)
 			printf("%d degrees turned\n", get_create_total_angle());
+		
 		create_stop();
 	}
 }
