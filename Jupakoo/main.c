201f44ab7959bb77b0e760b813867ef2936abed2
Jacob Adams <tookmund@gmail.com>
2015-04-16 21:12:18 -0400

    Add the rest of init stuff to setup

--- Jupakoo/main.c
+++ Jupakoo/main.c
@@ -2,9 +2,12 @@
 
 void setup()
 {
+	printf("Nooice\n");
 	//wait_for_light(0); 
 	//shut_down_in(119); // shut off the motors and stop the Create after 119 seconds
 	create_connect();
+	printf("Battery: %d\n",get_create_battery_capacity());
+	
 }
 
 void shutdown() 
@@ -15,9 +18,8 @@ void shutdown()
 
 int main()
 {
-	printf("Nooice\n");
+	
 	setup();
-	printf("Battery: %d\n",get_create_battery_capacity());
 	
 	turn(100,-75);
 	msleep(10);
