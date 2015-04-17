b2cf8c806b0db62038387f858dd86da1b5f85302
Jacob Adams <tookmund@gmail.com>
2015-04-16 20:19:23 -0400

    Write servo code

--- Jupakoo/jp.h
+++ Jupakoo/jp.h
@@ -2,9 +2,15 @@
 #ifndef _JP_H_
 #define _JP_H_
 
-#define openClaw 600
-#define closeClaw 1400
+#define claw 1
+#define arm 0
 
+#define OPEN 600
+#define CLOSE 1400
+
+#define UP 0
+#define DOWN 0
+#define ssp(port,position) set_servo_positon(port,position)
 void setup();
 
 //Speed is in mm/sec on interval [-500,500], distance is in mm;
